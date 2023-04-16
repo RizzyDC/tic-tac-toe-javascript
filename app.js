c3ee101a6731e569c867c344107db322a6d9ad50
RizzyDC <81084609+RizzyDC@users.noreply.github.com>
2023-04-16 10:34:37 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+/* Selecting all the elements with the id #gameboard and saving them to a const variable gameBoard */
+const gameBoard = document.querySelector('#gameboard');
+const infoDisplay = document.querySelector('#info');
+const startCells = [
+    '', '', '', '', '', '', '', '', '' 
+]
+
+let go = 'circle';
+infoDisplay.textContent = 'Circle goes first'
+
+function createBoard() {
+    startCells.forEach((_cell, index) => {
+    const cellElement = document.createElement('div');
+     cellElement.classList.add('square');
+     cellElement.id = index;
+     cellElement.addEventListener('click', addGo);
+     gameBoard.append(cellElement);
+    })
+}
+
+createBoard();
+
+function addGo(e) {
+const goDisplay = document.createElement('div')
+goDisplay.classList.add(go)
+e.target.append(goDisplay);
+go = go === 'circle' ? 'cross' : 'circle';
+infoDisplay.textContent = 'it is now' + go + ' \'s go.';
+e.target.removeEventListener('click', addGo);
+checkScore();
+}
+
+function checkScore() {
+    const winningCombos = [0, 1, 2];
+}
+
+ 
+
+
