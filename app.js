34e7bb58de82ca6b1020e6b25b18e975bf9a65b7
RizzyDC <81084609+RizzyDC@users.noreply.github.com>
2023-04-16 10:44:36 -0700

    Update app.js

--- app.js
+++ app.js
@@ -31,7 +31,13 @@ checkScore();
 }
 
 function checkScore() {
-    const winningCombos = [0, 1, 2];
+    const allSquares = document.querySelectorAll('.square');
+    console.log(allSquares)
+    const winningCombos = [
+        [0,1,2], [3,4,5], [6,7,8]
+        [0,3,6], [1,4,7], [2,5,8]
+        [0,4,8], [2,4,6]
+    ]
 }
 
  
